c4d04947ec47e72ff1e07e9cb62d8aafdb1f0305
Nipun Prodduturi <k308613@deluxe.com>
2022-04-04 09:15:31 -0500

    Add webpack

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def search
+    puts ">>>>>>>>>>>>>>>>>>>>"
     @users = User.search(params[:q])
-    render json: @users
+    # render json: @users
   end
 end
